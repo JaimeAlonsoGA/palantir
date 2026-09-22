857ffcf21260ff210b31b7dfb13a7447aae94bc9
Jaime Alonso García-Amorena <134053469+JaimeAlonsoGA@users.noreply.github.com>
2026-09-22 12:01:02 +0200

    lab.server CRUD

---
+++ src/server/lab.server.ts
@@ -0,0 +1,1 @@
+export { listEntries, getEntry, searchEntries, createEntry, patchEntry, upsertEntry, deleteEntry, listRefs, getRef, createRef, upsertRef, dumpAll, schemaDoc, catalog, llmsText } from './lab-core';
